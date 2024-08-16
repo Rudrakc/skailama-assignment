6ad17fd19789c0762562ed247fff8c227f2d7e7e
Rudrakc <rudrachauhan2888@gmail.com>
2024-08-17 01:42:26 +0530

    Remove Server folder from main branch

--- Server/env.js
+++ Server/env.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("Hii", typeof(process.env.MONGODB_URL));
-
 class Env {
   static PORT = process.env.PORT || 3000; //default port to listen
   static MONGODB_URL = process.env.MONGODB_URL; // MongoDB connection URI
