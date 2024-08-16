0056332db54a55a07385f95c6feff23fca8cfd8e
Rudrakc <rudrachauhan2888@gmail.com>
2024-08-17 01:44:56 +0530

    Remove Server folder from main branch

--- Server/routes/project.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createProject, getProjectById } from "../controllers/project.controller.js";
-
-const router = express.Router();
-
-router.get("/:id", getProjectById );
-router.post("/", createProject );
-
-export default router;
